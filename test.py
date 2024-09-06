7b7ca31d95abd6137a5db60c2861f1de0e89cb63
Mifikus <mifikus322@gmail.com>
2024-09-06 21:12:53 +0300

    Исправлена ошибка с тем, что бесконечно открывал один и тот же турнир

--- test.py
+++ test.py
@@ -1,18 +1,5 @@
-import re
-import time
-import win32gui
+from ClientLauncher.MainFunctions.windows import Windows
 
-a = 'Bigger €50 | €4,000 Gtd - 600/1,200 ante 150 - Tournament 3769315911 Table 4'
+a = Windows()
 
-
-#print(', '.join(re.split(r'^[^ |\|]+', a)[0:-1]))
-#print(a.split(', $'))
-
-#print(re.search(r', (€.+?) Gtd', a).group(1))
-
-print(re.search(r'\| (€.+?) Gtd', a).group(1))
-time.sleep(5)
-
-a = win32gui.GetForegroundWindow()
-
-print(win32gui.GetWindowText(a))
+a.open_instant_hand_history_menu()
