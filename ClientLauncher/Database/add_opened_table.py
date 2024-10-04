c9b4b50296bf23337b439465550304117514a66b
Mifikus <mifikus322@gmail.com>
2024-10-05 00:49:28 +0300

    Добавлена обработка ошибок БД

--- ClientLauncher/Database/add_opened_table.py
+++ ClientLauncher/Database/add_opened_table.py
@@ -18,18 +18,23 @@ class AddTable:
         if not table or not tournament_id:
             print(f'Нету айди турнира или номера стола чтобы добавить его в бд tournament_id {tournament_id} table {table}')
             return
-        _connection = mysql.connector.connect(host=HOST, user=USERNAME, password=PASSWORD)
-        _connection.autocommit = True
-        cursor = _connection.cursor()
+        while True:
+            try:
+                _connection = mysql.connector.connect(host=HOST, user=USERNAME, password=PASSWORD)
+                _connection.autocommit = True
+                cursor = _connection.cursor()
 
-        query = f"INSERT INTO {database_name}.opened_tables (tournament_id, table_num, script_name, date, is_collected) VALUES "\
-                f"('{tournament_id}', {table}, '{SCRIPT_NAME}', NOW(), 0);"
+                query = f"INSERT INTO {database_name}.opened_tables (tournament_id, table_num, script_name, date, is_collected) VALUES "\
+                        f"('{tournament_id}', {table}, '{SCRIPT_NAME}', NOW(), 0);"
 
-        print(f'add query {query}')
+                print(f'add query {query}')
 
-        cursor.execute(query)
+                cursor.execute(query)
 
-        _connection.disconnect()
+                _connection.disconnect()
+                break
+            except:
+                pass
 
     @staticmethod
     def set_table_collected_status(tournament_id: str, table: str):
@@ -37,15 +42,20 @@ class AddTable:
             print(f'Нету айди турнира или номера стола чтобы добавить его в бд tournament_id {tournament_id} table {table}')
             return
 
-        _connection = mysql.connector.connect(host=HOST, user=USERNAME, password=PASSWORD)
-        _connection.autocommit = True
-        cursor = _connection.cursor()
+        while True:
+            try:
+                _connection = mysql.connector.connect(host=HOST, user=USERNAME, password=PASSWORD)
+                _connection.autocommit = True
+                cursor = _connection.cursor()
 
-        query = f"UPDATE {database_name}.opened_tables SET "\
-                f"is_collected = 1 WHERE tournament_id = '{tournament_id}' AND table_num = {table};"
+                query = f"UPDATE {database_name}.opened_tables SET "\
+                        f"is_collected = 1 WHERE tournament_id = '{tournament_id}' AND table_num = {table};"
 
-        print(f'set_table_collected_status query {query}')
+                print(f'set_table_collected_status query {query}')
 
-        cursor.execute(query)
+                cursor.execute(query)
 
-        _connection.disconnect()
+                _connection.disconnect()
+                break
+            except:
+                pass
